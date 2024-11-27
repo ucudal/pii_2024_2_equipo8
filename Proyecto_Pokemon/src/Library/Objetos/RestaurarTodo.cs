16d2f88da76791a9c3360dd5ff13f4f98cae9d89
juanmafari <108095147+JuanOttonelli@users.noreply.github.com>
2024-11-27 09:27:14 -0300

    Comentarios en diseño de objeto

--- Proyecto_Pokemon/src/Library/Objetos/RestaurarTodo.cs
+++ Proyecto_Pokemon/src/Library/Objetos/RestaurarTodo.cs
@@ -8,7 +8,7 @@ public class RestaurarTodo: Objetos
     public RestaurarTodo() : base("Restaurar Todo") { }
     
     /// <summary>
-    /// metodo para usar el objeto en un pokemon, 
+    /// metodo para usar el objeto RestauraroTodo en un pokemon, vuelve la vida al nivel inicial y anula los efectos generados.
     /// </summary>
     public override string Usar(Pokemon pokemon, Entrenadores entrenador)
     {
@@ -16,10 +16,13 @@ public class RestaurarTodo: Objetos
         {
             if ((pokemon.Vida == pokemon.VidaBase))
             {
+                // Se evalua este caso para no perder el objeto si no se hace efectivo su uso
                 return $"No fue necesario restaurar, todos los valores al máximo.";
             }
         }
+        
         pokemon.Estado = null;
+        // Se usa VidaBase para tener una referencia de vida máxima para recuperar
         pokemon.Vida = pokemon.VidaBase;
 
         return $"{entrenador.Nombre} usó {Nombre} en {pokemon.Nombre}. Se recuperaron los efectos negativos y se restauró la vida incial.\n";
